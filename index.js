f468c676972633f1d8d88ba9463e8e45a5e6fbf9
Jelle Hak <jellehak@gmail.com>
2020-08-26 09:34:40 +0200

    fix

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import { AceEditor, AceEditorJson } from './src'
 export * from './src'
+
+function plugin (Vue) {
+  Vue.component('AceEditor', AceEditor)
+  Vue.component('AceEditorJson', AceEditorJson)
+}
+
+// Install by default if using the script tag
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+
+export default plugin
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import AceEditor from './AceEditor.vue'
 import AceEditorJson from './AceEditorJson.vue'
 
-function plugin (Vue) {
-  Vue.component('AceEditor', AceEditor)
-}
-
-// Install by default if using the script tag
-if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.use(plugin)
-}
-
-export default plugin
-
 // Export all components
 export {
   AceEditor,
